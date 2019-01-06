a3cb05e2ee429e1ccc7b3c18080e616593e1c4ca
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2019-01-06 21:44:52 +0100

    Add server

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const http = require("http");
+
+function start() {
+
+	function onRequest(request, response) {
+		console.log("Odebrano zapytanie");
+		response.writeHead(200, {"Content-type": "text/plain"});
+		response.write("works");
+		response.end();
+	}
+
+	let server = http.createServer(onRequest);
+	server.listen(9010);
+
+	console.log("uruchomionos serwer");
+}
+
+start();
